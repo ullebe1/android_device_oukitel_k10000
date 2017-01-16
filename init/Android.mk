e8049813ce9bc3b2a1b2f30e58835e09906e775d
Ulrik Djurtoft <ullebe1@gmail.com>
2017-01-16 20:33:57 +0100

    Did some more work
    Change-Id: Id89e65d1849968bf0bdb6fd8db33673539e2ac80

--- init/Android.mk
+++ init/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
-LOCAL_SRC_FILES := init_porridge.cpp
-LOCAL_MODULE := libinit_porridge
+LOCAL_SRC_FILES := init_k10000.cpp
+LOCAL_MODULE := libinit_k10000
 include $(BUILD_STATIC_LIBRARY)
